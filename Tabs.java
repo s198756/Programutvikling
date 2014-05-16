a75576a2a9698ba0f851eeb1f7069327a1d506a1
Tscorpio1701 <tscorpio1701@gmail.com>
2014-05-16 14:18:55 +0200

    Endringer

--- Tabs.java
+++ Tabs.java
@@ -1,67 +1,65 @@
-
 /**
  * Created by Thomas Newman, s198753 on 05.05.2014.
- * Formålet med denne klassen er å samle alle JPanels vi har laget og legge dem inn i en JFrame
  */
+
 import javax.swing.*;
 import java.awt.*;
-import java.net.URL;
 import java.sql.SQLException;
 
+public class Tabs extends JFrame {
+    JFrame frame = new JFrame("Boligfirma");
+    JTabbedPane tabbedPane = new JTabbedPane();
 
+    ContractUI cUI;
+    ContracttableWindow fc;
+    PersonUI pUI;
+    PersontableWindow fp;
+    DwellingUnitUI rUI;
+    DwellingtableWindow fr;
 
-public class Tabs extends JFrame
-{
-    private JFrame frame;
-    private JTabbedPane tabbedPane;
-    private ContractUI cUI;
-    private PersonUI pUI;
-    private DwellingUnitUI dUI;
-    private Person person;
-    private Contract contract;
-    private DwellingUnit dwellingUnit;
+    public Tabs() throws SQLException {
 
+        Person person = new Person();
+        DwellingUnit dwellingUnit = new DwellingUnit();
+        Contract contract = new Contract();
 
-    public Tabs() throws SQLException
-    {
-        //Implementerer de tre klassene
-        person = new Person();
-        contract = new Contract();
-        dwellingUnit = new DwellingUnit();
-
-        // Konstruktør for å sette opp JTabbedPane og legger til content fra JPanels og klasser.
-        tabbedPane = new JTabbedPane();
         cUI = new ContractUI(contract, person, dwellingUnit);
+        fp = new PersontableWindow();
         pUI = new PersonUI(person);
-        dUI = new DwellingUnitUI(dwellingUnit);
+        fr = new DwellingtableWindow();
+        rUI = new DwellingUnitUI(dwellingUnit);
+        fc = new ContracttableWindow();
 
-        tabbedPane.addTab("Kontrakt", cUI);
-        tabbedPane.addTab("Person", pUI);
-        tabbedPane.addTab("Bolig", dUI);
+        tabbedPane.add("Finn Person", fp );
+        frame.getContentPane();
 
-        // Konstruktør som legger til tabbedPane og setter opp JFramens størrelse
-        frame = new JFrame("Boligformidling");
-        frame.getContentPane().add(tabbedPane, BorderLayout.CENTER);
+        tabbedPane.add("Person", pUI);
+        frame.getContentPane();
+
+        tabbedPane.add("Finn Bolig", fr);
+        frame.getContentPane();
+
+        tabbedPane.add("Bolig", rUI);
+        frame.getContentPane();
+
+        tabbedPane.add("Finn Kontrakt", fc);
+        frame.getContentPane();
+
+        tabbedPane.add("Kontrakt", cUI);
+        frame.getContentPane();
+
+        frame.getContentPane().add(tabbedPane);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(1280, 720);
+        frame.setPreferredSize(new Dimension(1280, 720));
+        frame.pack();
+        frame.setLocationByPlatform(true);
         frame.setVisible(true);
-        frame.validate();
-
-
-        // Konstruktør som skal legge inn program ikon.
-        String bildefil = "Bilder/PrIcon2.png";
-        URL kilde = Tabs.class.getResource(bildefil);
-        if (kilde != null) {
-            ImageIcon bilde = new ImageIcon(kilde);
-            Image ikon = bilde.getImage();
-            frame.setIconImage(ikon);
-        }
     }
 
-    // Main metode for programmet
-    public static void main(String[] args) throws SQLException
-    {
+    public static void main(String[] args) throws SQLException {
         new Tabs();
     }
-
 }
+
+
+
